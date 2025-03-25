486cff9644f8b7b1c162bc5adeb616ccac99846c
linjiongming <linjiongming@163.com>
2025-03-25 11:58:58 +0800

    添加项目文件。

---
+++ FirebaseAdmin.Extensions/FirebaseMessagingExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Google.Apis.Json;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FirebaseAdmin.Messaging;
+
+public static class FirebaseMessagingExtensions
+{
+    public static async Task<string> SendAsync(this FirebaseMessaging firebaseMessaging, [NotNull] string? json, CancellationToken cancellation = default)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(json);
+        var message = NewtonsoftJsonSerializer.Instance.Deserialize<Message>(json);
+        return await firebaseMessaging.SendAsync(message, cancellation);
+    }
+    public static async Task<BatchResponse> SendEachAsync(this FirebaseMessaging firebaseMessaging, IEnumerable<string> jsons, CancellationToken cancellation = default)
+    {
+        var messages = jsons.Select(NewtonsoftJsonSerializer.Instance.Deserialize<Message>);
+        return await firebaseMessaging.SendEachAsync(messages, cancellation);
+    }
+    public static string ToJson(this Message message) => NewtonsoftJsonSerializer.Instance.Serialize(message);
+}
---
+++ FirebaseAdmin.Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddGoogleCredential(this IServiceCollection services, [NotNull] string? credentialPath)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(credentialPath);
+        if (!Path.IsPathRooted(credentialPath))
+        {
+            credentialPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, credentialPath);
+        }
+        services.AddSingleton(GoogleCredential.FromFile(credentialPath));
+        return services;
+    }
+    public static IServiceCollection AddFirebaseApp(this IServiceCollection services)
+    {
+        services.AddSingleton(provider =>
+        {
+            var credential = provider.GetRequiredService<GoogleCredential>();
+            var options = new AppOptions
+            {
+                Credential = credential,
+                ProjectId = ((ServiceAccountCredential)credential.UnderlyingCredential).ProjectId,
+            };
+            return FirebaseApp.Create(options, AppDomain.CurrentDomain.FriendlyName);
+        });
+        return services;
+    }
+    public static IServiceCollection AddFirebaseMessaging(this IServiceCollection services)
+    {
+        if (!services.Any(x => x.ServiceType == typeof(FirebaseApp)))
+        {
+            services.AddFirebaseApp();
+        }
+        services.AddSingleton(provider => FirebaseMessaging.GetMessaging(provider.GetRequiredService<FirebaseApp>()));
+        return services;
+    }
+}
